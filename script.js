c509ca924f453353c895b0a91393cf4eecc35739
SAHIL THAKUR <sahil20120067926thakur@gmail.com>
2025-08-19 21:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
   // your code here
+	let chunks = [];
+	let i=0;
+	while(i<str.length){
+		let subStr = str.slice(i, i+size);
+		chunks.push(subStr);
+		i+=size;
+	}
+	return chunks;
 }
 
 // Do not change the code below
